cbe64e8cb74c5ddfd98dc977f590dc5a4a5c7227
Sergei Antipov <greendayonfire@gmail.com>
2017-05-01 15:09:08 +0700

    Disable chromedriver verbose mode

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,5 @@
 export DISPLAY=:10
 Xvfb ${DISPLAY} -screen 0 1366x768x24 +extension RANDR -ac &
 service dbus restart
-chromedriver  --whitelisted-ips="" --verbose --log-path=/robotest/state/chromedriver.log &
+chromedriver  --whitelisted-ips="" &
 robotest 2>&1 $@
