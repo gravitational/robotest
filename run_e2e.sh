0d1fa0847f48d7a6a1049aae445ade85a6ab725e
Denis Mishin <denis.s.mishin@gmail.com>
2017-05-18 21:29:12 -0400

    remove temp files

--- run_e2e.sh
+++
@@ -1,9 +0,0 @@
-export P='/Users/denismishin/src/gravi/robotest' 
-docker run \
-	-v ${P}/wd/gravity:/usr/bin/gravity \
-	-v ${P}/wd:/robotest \
-	-v ${P}/build/robotest-e2e:/usr/bin/robotest-e2e \
-	-v ${P}/assets/terraform:/robotest/terraform \
-	quay.io/gravitational/robotest-e2e:1.0.35 \
-	-config /robotest/config/config.azure.yaml \
-	-ginkgo.focus="Onprem Installation" -debug -mode=wizard
--- run_suite.sh
+++
@@ -1,8 +0,0 @@
-export P='/Users/denismishin/src/gravi/robotest' 
-docker run \
-	-v ${P}/wd_suite:/robotest \
-	-v ${P}/build/robotest-suite:/usr/bin/robotest-suite \
-	-v ${P}/assets/terraform:/robotest/terraform \
-	quay.io/gravitational/robotest-suite:1.0.35 \
-	-config /robotest/config/config.yaml \
-	-dir=/robotest/state -tag=robotest-west-1
