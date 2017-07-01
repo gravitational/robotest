3290e5d33572aa394a0337b9d8c83454177d4c97
Denis Mishin <denis.s.mishin@gmail.com>
2017-06-30 21:24:31 -0400

    run script moved to docker

--- docker/suite/run_suite.sh
+++ docker/suite/run_suite.sh
@@ -1,15 +1,18 @@
-#!/bin/bash +x
+#!/bin/bash
+
+set -eu -o pipefail
 
 #
-# could be local .tar installer or s3:// or http(s) URL
+# installer could be local .tar installer or s3:// or http(s) URL
 #
-INSTALLER_URL=${INSTALLER_URL:-$1}
+
+if [ -f $INSTALLER_URL ] ; then
+	INSTALLER_FILE = '/robotest/installer.tar'
+fi
 
 # OS could be ubuntu,centos,rhel 
 # storage driver: could be devicemapper,loopback,overlay,overlay2 
 #  separate multiple values by comma for OS and storage driver
-RUN_TEST=${RUN_TEST:-$2}
-
 TEST_OS=${TEST_OS:-ubuntu}
 STORAGE_DRIVER=${STORAGE_DRIVER:-devicemapper}
 
@@ -27,18 +30,15 @@ DESTROY_ON_SUCCESS=${DESTROY_ON_SUCCESS:-true}
 DESTROY_ON_FAILURE=${DESTROY_ON_FAILURE:-true}
 
 # PIN robotest version if needed
-ROBOTEST_VERSION=${ROBOTEST_VERSION:-latest}
+ROBOTEST_VERSION=${ROBOTEST_VERSION:-stable}
 
-# which cloud to use : aws or azure
-# define rest of the keys in the CLOUD_CONFIG below
-DEPLOY_TO=${DEPLOY_TO:-azure}
+if [ $DEPLOY_TO != "azure" ] && [ $DEPLOY_TO != "aws" ] ; then
+	echo "Unsupported deployment cloud ${DEPLOY_TO}"
+	exit 1
+fi
 
-CLOUD_CONFIG="
-installer_url: ${INSTALLER_URL}
-script_path: /robotest/terraform/${DEPLOY_TO}
-state_dir: /robotest/state
-cloud: ${DEPLOY_TO}
-aws:
+if [ $DEPLOY_TO == "aws" ] || [[ $INSTALLER_URL = 's3://'* ]] ; then
+AWS_CONFIG="aws:
   access_key: ${AWS_ACCESS_KEY}
   secret_key: ${AWS_SECRET_KEY}
   ssh_user: ubuntu
@@ -46,8 +46,11 @@ aws:
   key_pair: ${AWS_KEYPAIR}
   region: ${AWS_REGION}
   vpc: Create New
-  docker_device: /dev/xvdb
-azure: 
+  docker_device: /dev/xvdb"
+fi
+
+if [ $DEPLOY_TO == "azure" ] ; then 
+AZURE_CONFIG="azure: 
   subscription_id: ${AZURE_SUBSCRIPTION_ID}
   client_id: ${AZURE_CLIENT_ID}
   client_secret: ${AZURE_CLIENT_SECRET}
@@ -57,57 +60,67 @@ azure:
   ssh_user: robotest
   key_path: /robotest/config/ops.pem
   authorized_keys_path: /robotest/config/ops_rsa.pub
-  docker_device: /dev/sdd
-"
-check_args () {
-	for v in $@ 
-	do
-		if [ -z "${!v}" ] ; then 
-			echo "variable \$$v must be set"
+  docker_device: /dev/sdd"
+fi
+
+check_files () {
+	ABORT=
+	for v in $@ ; do
+		if [ ! -f "${v}" ] ; then 
+			echo "${v} does not exist"
 			ABORT=true
 		fi
 	done
 
 	if [ ! -z $ABORT ] ; then 
-		echo 'Usage: run.sh installer_url tests' ;
 		exit 1 ;
 	fi
-
 }
 
-check_args INSTALLER_URL RUN_TEST SSH_KEY SSH_PUB 
+check_files ${SSH_KEY} ${SSH_PUB}
 
-if [ $DEPLOY_TO == "aws" ]; then
-	check_args AWS_REGION AWS_KEYPAIR AWS_SECRET_KEY AWS_ACCESS_KEY
-elif [ $DEPLOY_TO == "azure" ]; then 
-	check_args AZURE_TENANT_ID AZURE_CLIENT_SECRET AZURE_CLIENT_ID AZURE_SUBSCRIPTION_ID
-else
-	echo Unsupported deployment cloud $DEPLOY_TO
+if [ -n "$GCL_PROJECT_ID" ] ; then
+	check_files ${GOOGLE_APPLICATION_CREDENTIALS}
+	GCP_CONFIG=$'gcp:\n  project_id: '${GCL_PROJECT_ID}
 fi
 
-#
-# you generally don't need to change anything beyond this line
-# 
+CLOUD_CONFIG="
+installer_url: ${INSTALLER_FILE:-${INSTALLER_URL}}
+script_path: /robotest/terraform/${DEPLOY_TO}
+state_dir: /robotest/state
+cloud: ${DEPLOY_TO}
+${AWS_CONFIG:-}
+${AZURE_CONFIG:-}
+${GCP_CONFIG:-}
+"
+
+# will make verbose logging to console
+LOG_CONSOLE=${LOG_CONSOLE:-'-test.v'}
+
 
 P=$(pwd)
 export REPORT_FILE=$(date '+%m%d-%H%M')
 mkdir -p ${P}/wd_suite/state/${TAG}
 
-set -o xtrace
+set -o xtrace 
 
 docker run \
 	-v ${P}/wd_suite/state:/robotest/state \
 	-v ${SSH_KEY}:/robotest/config/ops.pem \
 	-v ${SSH_PUB}:/robotest/config/ops_rsa.pub \
 	${ROBOTEST_DEV:+'-v' "${P}/build/robotest-suite:/usr/bin/robotest-suite"} \
+	${INSTALLER_FILE:+'-v' "${INSTALLER_URL}:${INSTALLER_FILE}"} \
+	${GCL_PROJECT_ID:+'-v' "${GOOGLE_APPLICATION_CREDENTIALS}:/robotest/config/gcp.json" '-e' 'GOOGLE_APPLICATION_CREDENTIALS=/robotest/config/gcp.json'} \
 	quay.io/gravitational/robotest-suite:${ROBOTEST_VERSION} \
-	robotest-suite -test.timeout=48h -test.v \
+	robotest-suite -test.timeout=48h ${LOG_CONSOLE} \
 	-test.parallel=${PARALLEL_TESTS} -repeat=${REPEAT_TESTS} -fail-fast=false \
-	-provision="$CLOUD_CONFIG" -always-collect-logs=${ALWAYS_COLLECT_LOGS} \
+	-provision="${CLOUD_CONFIG}" -always-collect-logs=${ALWAYS_COLLECT_LOGS} \
 	-resourcegroup-file=/robotest/state/alloc.txt \
 	-destroy-on-success=${DESTROY_ON_SUCCESS} -destroy-on-failure=${DESTROY_ON_FAILURE}  \
 	-tag=${TAG} -suite=sanity -os=${TEST_OS} -storage-driver=${STORAGE_DRIVER} \
-	"${RUN_TEST}" \
+	$@ \
 	2>&1 | tee ${P}/wd_suite/state/${TAG}/${REPORT_FILE}.txt
 
-echo ${P}/wd_suite/state/${REPORT_FILE}.txt
+set +o xtrace
+
+echo "Operation log in ${P}/wd_suite/state/${REPORT_FILE}.txt"
