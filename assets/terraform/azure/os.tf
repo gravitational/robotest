564c9117852393efd25dbd7d2368b67360e68332
Denis Mishin <denis.s.mishin@gmail.com>
2017-10-17 23:52:57 -0400

    tabs to spaces

--- assets/terraform/azure/os.tf
+++ assets/terraform/azure/os.tf
@@ -5,60 +5,60 @@
 #
 
 variable "os_publisher" {
-	type = "map"
+    type = "map"
 
-	default = {
-		"ubuntu" = "Canonical"
-		"redhat" = "RedHat"
-		"centos" = "OpenLogic"
-		"debian" = "credativ"
-		"suse"	 = "SUSE"
-	}
+    default = {
+        "ubuntu" = "Canonical"
+        "redhat" = "RedHat"
+        "centos" = "OpenLogic"
+        "debian" = "credativ"
+        "suse"   = "SUSE"
+    }
 }
 
 variable "os_offer" {
-	type = "map"
+    type = "map"
 
-	default = {
-		"ubuntu" = "UbuntuServer"
-		"redhat" = "RHEL"
-		"centos" = "CentOS"
-		"debian" = "Debian"
-		"suse" 	 = "SLES"
-	}
+    default = {
+        "ubuntu" = "UbuntuServer"
+        "redhat" = "RHEL"
+        "centos" = "CentOS"
+        "debian" = "Debian"
+        "suse"   = "SLES"
+    }
 }
 
 variable "os_sku" {
-	type = "map"
+    type = "map"
 
-	default = {
-		"ubuntu" 	 = "16.04-LTS"
-		"redhat" 	 = "7.3"
-		"redhat:7.2" = "7.2"
-		"redhat:7.3" = "7.3"
-		"redhat:7.4" = "7-RAW"
-		"centos" 	 = "7.3"
-		"centos:7.2" = "7.2"
-		"centos:7.3" = "7.3"
-		"debian" 	 = "8"
-		"suse"		 = "12-SP3"
-	}
+    default = {
+        "ubuntu"     = "16.04-LTS"
+        "redhat"     = "7.3"
+        "redhat:7.2" = "7.2"
+        "redhat:7.3" = "7.3"
+        "redhat:7.4" = "7-RAW"
+        "centos"     = "7.3"
+        "centos:7.2" = "7.2"
+        "centos:7.3" = "7.3"
+        "debian"     = "8"
+        "suse"       = "12-SP3"
+    }
 }
 
 variable "os_version" {
-	type = "map"
+    type = "map"
 
-	default = {
-		"ubuntu" 	 = "16.04.201708151"
-		"redhat" 	 = "7.3.2017090723"
-		"redhat:7.3" = "7.3.2017090723"
-		"redhat:7.2" = "latest"
-		"redhat:7.4" = "7.4.2017080923"
-		"centos" 	 = "latest"
-		"centos:7.4" = "7.4.20170919"
-		"centos:7.3" = "7.3.20170925"
-		"centos:7.2" = "7.2.20170517"
-		"debian" 	 = "latest"
-		"suse"		 = "2017.09.07"
-	}
+    default = {
+        "ubuntu"     = "16.04.201708151"
+        "redhat"     = "7.3.2017090723"
+        "redhat:7.3" = "7.3.2017090723"
+        "redhat:7.2" = "latest"
+        "redhat:7.4" = "7.4.2017080923"
+        "centos"     = "latest"
+        "centos:7.4" = "7.4.20170919"
+        "centos:7.3" = "7.3.20170925"
+        "centos:7.2" = "7.2.20170517"
+        "debian"     = "latest"
+        "suse"       = "2017.09.07"
+    }
 }
