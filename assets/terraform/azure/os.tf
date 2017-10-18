58916d016e71d6c677434dfb77b471c354d37afe
Denis Mishin <denis.s.mishin@gmail.com>
2017-10-17 22:07:57 -0400

    support OS versions

--- assets/terraform/azure/os.tf
+++ assets/terraform/azure/os.tf
@@ -32,7 +32,12 @@ variable "os_sku" {
 	default = {
 		"ubuntu" = "16.04-LTS"
 		"redhat" = "7.3"
+		"redhat:7.2" = "7.2"
+		"redhat:7.3" = "7.3"
+		"redhat:7.4" = "7-RAW"
 		"centos" = "7.3"
+		"centos:7.2" = "7.2"
+		"centos:7.3" = "7.3"
 		"debian" = "8"
 	}
 }
@@ -41,9 +46,15 @@ variable "os_version" {
 	type = "map"
 
 	default = {
-		"ubuntu" = "16.04.201708151"
-		"redhat" = "7.3.2017090723"
-		"centos" = "latest"
-		"debian" = "latest"
+		"ubuntu" 	 = "16.04.201708151"
+		"redhat" 	 = "7.3.2017090723"
+		"redhat:7.3" = "7.3.2017090723"
+		"redhat:7.2" = "latest"
+		"redhat:7.4" = "7.4.2017080923"
+		"centos" 	 = "latest"
+		"centos:7.4" = "7.4.20170919"
+		"centos:7.3" = "7.3.20170925"
+		"centos:7.2" = "7.2.20170517"
+		"debian" 	 = "latest"
 	}
 }
