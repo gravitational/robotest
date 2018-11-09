0b934addefa212cf65ce3ebf394d6113b67c1a6a
dmitri <deemok@gmail.com>
2018-11-09 22:29:28 +0100

    Disable cidr outputs

--- assets/terraform/gce/output.tf
+++ assets/terraform/gce/output.tf
@@ -10,10 +10,11 @@ output "public_ips" {
   value = ["${google_compute_instance.node.*.network_interface.0.access_config.0.assigned_nat_ip}"]
 }
 
-output "pod_cidr_blocks" {
-  value = ["${google_compute_instance.node.*.network_interface.0.alias_ip_range.0.ip_cidr_range}"]
-}
+# output "pod_cidr_blocks" {
+#   value = ["${google_compute_instance.node.*.network_interface.0.alias_ip_range.0.ip_cidr_range}"]
+# }
+# 
+# output "service_cidr_blocks" {
+#   value = ["${google_compute_instance.node.*.network_interface.0.alias_ip_range.1.ip_cidr_range}"]
+# }
 
-output "service_cidr_blocks" {
-  value = ["${google_compute_instance.node.*.network_interface.0.alias_ip_range.1.ip_cidr_range}"]
-}
