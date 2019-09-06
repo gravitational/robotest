b3a3bae4165ed28f7c08e3b6cd9031f025a34a07
Bernard Kim <bernard@gravitational.com>
2019-09-06 15:07:15 -0700

    Add CPU and Memory field to libvirt config

--- infra/providers/libvirt/config.go
+++ infra/providers/libvirt/config.go
@@ -10,4 +10,8 @@ type Config struct {
 	SSHPublicKeyPath string `json:"ssh_pub_key_path" yaml:"ssh_pub_key_path" validate:"required"`
 	// DockerDevice specifies the block device for Docker
 	DockerDevice string `json:"docker_device,omitempty" yaml:"docker_device"`
+	// CPU specifies the number of vCPUs per machine
+	CPU int `json:"cpu" yaml:"cpu" validate:"required"`
+	// Memory specifies the amount of memory per machine in MB
+	Memory int `json:"memory" yaml:"memory" validate:"required"`
 }
