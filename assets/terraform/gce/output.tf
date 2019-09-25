7bea45d1b9e77cd7f49ffd9b2997ec0f9fc570f5
dmitri <deemok@gmail.com>
2019-09-25 17:53:37 +0200

    Fix outputs to conform to terraform 0.12+. Fix bootstrap script for redhat/centos to retry SELinux config if lock is taken.

--- assets/terraform/gce/output.tf
+++ assets/terraform/gce/output.tf
@@ -3,17 +3,9 @@
 #
 
 output "private_ips" {
-  value = [google_compute_instance.node.*.network_interface.0.network_ip]
+  value = google_compute_instance.node.*.network_interface.0.network_ip
 }
 
 output "public_ips" {
-  value = [google_compute_instance.node.*.network_interface.0.access_config.0.nat_ip]
+  value = google_compute_instance.node.*.network_interface.0.access_config.0.nat_ip
 }
-
-# output "pod_cidr_blocks" {
-#   value = ["${google_compute_instance.node.*.network_interface.0.alias_ip_range.0.ip_cidr_range}"]
-# }
-# 
-# output "service_cidr_blocks" {
-#   value = ["${google_compute_instance.node.*.network_interface.0.alias_ip_range.1.ip_cidr_range}"]
-# }
