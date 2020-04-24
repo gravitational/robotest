137c4df6d51541e6a7c42535d5a5344540681b0f
a-palchikov <deemok@gmail.com>
2020-04-24 12:57:04 +0200

    Update images on GCE (#208)
    * Update images on GCE
    * Remove lvm2 and devicemapper packages
    * Address review comments

--- assets/terraform/gce/os.tf
+++ assets/terraform/gce/os.tf
@@ -10,17 +10,21 @@ variable "oss" {
 
   default = {
     # os -> {project}/{image}
-    "ubuntu:16"     = "ubuntu-os-cloud/ubuntu-1604-xenial-v20190816"
-    "ubuntu:18"     = "ubuntu-os-cloud/ubuntu-1804-bionic-v20190813a"
-    "ubuntu:latest" = "ubuntu-os-cloud/ubuntu-1804-bionic-v20190813a"
-    "redhat:7"      = "rhel-cloud/rhel-7-v20190813"
-    "centos:7"      = "centos-cloud/centos-7-v20190813"
-    "debian:8"      = "debian-cloud/debian-8-jessie-v20180401"
-    "debian:9"      = "debian-cloud/debian-9-stretch-v20190813"
-    "debian:latest" = "debian-cloud/debian-9-stretch-v20190813"
-    "suse:12"       = "suse-cloud/sles-12-sp4-v20190617"
-    "suse:15"       = "suse-cloud/sles-15-sp1-v20190828"
-    "suse:latest"   = "suse-cloud/sles-15-sp1-v20190828"
+    "ubuntu:16"     = "ubuntu-os-cloud/ubuntu-1604-xenial-v20200407"
+    "ubuntu:18"     = "ubuntu-os-cloud/ubuntu-1804-bionic-v20200414"
+    "ubuntu:20"     = "ubuntu-os-cloud/ubuntu-2004-focal-v20200423"
+    "ubuntu:latest" = "ubuntu-os-cloud/ubuntu-2004-focal-v20200423"
+    "redhat:7"      = "rhel-cloud/rhel-7-v20200420"
+    "redhat:8"      = "rhel-cloud/rhel-8-v20200413"
+    "centos:7"      = "centos-cloud/centos-7-v20200420"
+    "centos:8"      = "centos-cloud/centos-8-v20200413"
+    "debian:8"      = "debian-cloud/debian-8-jessie-v20180611"
+    "debian:9"      = "debian-cloud/debian-9-stretch-v20200420"
+    "debian:10"     = "debian-cloud/debian-10-buster-v20200413"
+    "debian:latest" = "debian-cloud/debian-10-buster-v20200413"
+    "suse:12"       = "suse-cloud/sles-12-sp5-v20200227"
+    "suse:15"       = "suse-cloud/sles-15-sp1-v20200415"
+    "suse:latest"   = "suse-cloud/sles-15-sp1-v20200415"
   }
 }
 
