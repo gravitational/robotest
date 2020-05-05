8eabb3779340064066d35476766ea55256926b48
Walt Della <walt@gravitational.com>
2020-05-04 22:43:30 -0700

    Pass `os` and `nodes` via robotest.tfvars.json.
    These two parameters were previously handled as cli flags. This meant we
    had two paths for routing data, and more annoyingly: it wasn't easy to
    recreate a robotest `terraform apply` or `terraform destroy` via the
    command line because these two values needed to be manually specified in
    addition to the other values that came from robotest.tfvars.json.

---
+++ infra/terraform/terraform_test.go
@@ -0,0 +1,98 @@
+package terraform
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/gravitational/robotest/infra/providers/gce"
+)
+
+func TestConvertConfigToTerraformVars(t *testing.T) {
+	gceConfig := gce.Config{
+		Credentials:      "/robotest/gce-creds.json",
+		VMType:           "excellent",
+		SSHUser:          "ubuntu",
+		SSHPublicKeyPath: "/robotest/.ssh/robo.pub",
+		SSHKeyPath:       "/robotest/.ssh/robo",
+		NodeTag:          "unittest",
+	}
+	cfg := Config{
+		CloudProvider: "gce",
+		GCE:           &gceConfig,
+		OS:            "ubuntu:20",
+		ScriptPath:    "/robotest/assets/terraform/gce",
+		NumNodes:      3,
+		InstallerURL:  "s3://hub.gravitational.io/gravity/oss/app/telekube/7.0.0/linux/x86_64/telekube-7.0.0-linux-x86_64.tar",
+		DockerDevice:  "/dev/xvdb",
+		VarFilePath:   "/robotest/custom-vars.json",
+	}
+	cfg.Validate()
+
+	configMap, err := configToTerraformVars(cfg)
+	if err != nil {
+		t.Error(err)
+	}
+	expected := make(map[string]interface{})
+	expected["credentials"] = "/robotest/gce-creds.json"
+	expected["nodes"] = 3
+	expected["vm_type"] = "excellent"
+	expected["os"] = "ubuntu:20"
+	expected["os_user"] = "ubuntu"
+	expected["ssh_pub_key_path"] = "/robotest/.ssh/robo.pub"
+	expected["node_tag"] = "unittest"
+
+	b, err := json.Marshal(configMap)
+	e, err := json.Marshal(expected)
+	if !reflect.DeepEqual(b, e) {
+		t.Errorf("\ngot:\t\t%q\nexpected:\t%q", b, e)
+	}
+}
+
+func TestConvertConfigToTerraformVarsOptionalValues(t *testing.T) {
+	gceConfig := gce.Config{
+		Region:           "us-west1",
+		Zone:             "us-west-1-b",
+		Project:          "unittesting",
+		Credentials:      "/robotest/gce-creds.json",
+		VMType:           "excellent",
+		SSHUser:          "ubuntu",
+		SSHPublicKeyPath: "/robotest/.ssh/robo.pub",
+		SSHKeyPath:       "/robotest/.ssh/robo",
+		NodeTag:          "unittest",
+	}
+	cfg := Config{
+		CloudProvider: "gce",
+		GCE:           &gceConfig,
+		OS:            "ubuntu:20",
+		ScriptPath:    "/robotest/assets/terraform/gce",
+		NumNodes:      3,
+		InstallerURL:  "s3://hub.gravitational.io/gravity/oss/app/telekube/7.0.0/linux/x86_64/telekube-7.0.0-linux-x86_64.tar",
+		DockerDevice:  "/dev/xvdb",
+		VarFilePath:   "/robotest/custom-vars.json",
+	}
+	cfg.Validate()
+
+	configMap, err := configToTerraformVars(cfg)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := make(map[string]interface{})
+	expected["region"] = "us-west1"
+	expected["zone"] = "us-west-1-b"
+	expected["project"] = "unittesting"
+	expected["credentials"] = "/robotest/gce-creds.json"
+	expected["nodes"] = 3
+	expected["vm_type"] = "excellent"
+	expected["os"] = "ubuntu:20"
+	expected["os_user"] = "ubuntu"
+	expected["ssh_pub_key_path"] = "/robotest/.ssh/robo.pub"
+	expected["node_tag"] = "unittest"
+
+	b, err := json.Marshal(configMap)
+	e, err := json.Marshal(expected)
+	if !reflect.DeepEqual(b, e) {
+		t.Errorf("\ngot:\t\t%q\nexpected:\t%q", b, e)
+	}
+}
