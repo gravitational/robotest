f7c96a37eda2db87033dd929de5643f6199e655c
Walt Della <walt@gravitational.com>
2020-05-21 19:11:10 -0700

    Refactor expand and shrink codepaths.
    In preparation for shrink work (#188), this patch refactors Expand and
    the new Shrink to have consistent signatures.  It also adds JoinNode and
    retouches RemoveNode to be consistent & independently useful singular
    pieces of code.
    
    The main motivation for this is the fact that shrink is not
    parallelizable:
    
      https://github.com/gravitational/robotest/pull/229#discussion_r428221568
    
    Expand/Join was already serializied, and the signature reworked for
    consistency.

--- infra/gravity/cluster_resize.go
+++ infra/gravity/cluster_resize.go
@@ -3,47 +3,33 @@ package gravity
 import (
 	"context"
 
-	"github.com/gravitational/robotest/lib/constants"
-	"github.com/gravitational/robotest/lib/utils"
-
 	"github.com/gravitational/trace"
-	"github.com/sirupsen/logrus"
 )
 
-func (c *TestContext) Expand(current, extra []Gravity, p InstallParam) error {
-	if len(current) == 0 || len(extra) == 0 {
+// Expand joins one or more node to a cluster
+func (c *TestContext) Expand(currentCluster, nodesToJoin []Gravity, p InstallParam) error {
+	if len(currentCluster) < 1 {
 		return trace.BadParameter("empty node list")
 	}
-	if c.provisionerCfg.CloudProvider == constants.Ops {
-		return trace.NotImplemented("not implemented")
+	if len(nodesToJoin) < 1 { // nothing to be done
+		return nil
 	}
 
-	c.Logger().WithFields(logrus.Fields{
-		"current": current,
-		"extra":   extra,
-	}).Info("Expand.")
-
+	// status is solely used for gathering the join token, can this be replaced
+	// with InstallParam.Token -- 2020-05 walt
+	peer := currentCluster[0]
 	ctx, cancel := context.WithTimeout(c.ctx, c.timeouts.Status)
 	defer cancel()
-
-	master := current[0]
-	joinAddr := master.Node().PrivateAddr()
-	status, err := master.Status(ctx)
+	status, err := peer.Status(ctx)
 	if err != nil {
-		return trace.Wrap(err, "query status from [%v]", master)
+		return trace.Wrap(err, "Query status from [%v]: %v", peer, err)
 	}
+	token := status.Cluster.Token.Token
 
-	ctx, cancel = context.WithTimeout(c.ctx, withDuration(c.timeouts.Install, len(extra)))
-	defer cancel()
+	c.Logger().WithField("current", currentCluster).WithField("extra", nodesToJoin).Info("Expand.")
 
-	for _, node := range extra {
-		c.Logger().WithField("node", node).Info("Join.")
-		err = node.Join(ctx, JoinCmd{
-			PeerAddr: joinAddr,
-			Token:    status.Cluster.Token.Token,
-			Role:     p.Role,
-			StateDir: p.StateDir,
-		})
+	for _, node := range nodesToJoin {
+		err = c.joinNode(peer, node, token, p)
 		if err != nil {
 			return trace.Wrap(err, "error joining cluster on node %s: %v", node.String(), err)
 		}
@@ -52,38 +38,67 @@ func (c *TestContext) Expand(current, extra []Gravity, p InstallParam) error {
 	return nil
 }
 
-// ShrinkLeave will gracefully leave cluster
-func (c *TestContext) ShrinkLeave(nodesToKeep, nodesToRemove []Gravity) error {
-	ctx, cancel := context.WithTimeout(c.ctx, withDuration(c.timeouts.Leave, len(nodesToRemove)))
+// JoinNode has one node join a peer already in a cluster
+func (c *TestContext) JoinNode(peer, nodeToJoin Gravity, p InstallParam) error {
+
+	ctx, cancel := context.WithTimeout(c.ctx, c.timeouts.Status)
 	defer cancel()
-
-	errs := make(chan error, len(nodesToRemove))
-	for _, node := range nodesToRemove {
-		go func(n Gravity) {
-			err := n.Leave(ctx, Graceful(true))
-			errs <- trace.Wrap(err, n.Node().PrivateAddr())
-		}(node)
+	status, err := peer.Status(ctx)
+	if err != nil {
+		return trace.Wrap(err, "Query status from [%v]: %v", peer, err)
 	}
-
-	return trace.Wrap(utils.CollectErrors(ctx, errs))
+	err = c.joinNode(peer, nodeToJoin, status.Cluster.Token.Token, p)
+	return trace.Wrap(err)
 }
 
-// RemoveNode simulates sudden nodes loss within an existing cluster followed by node eviction
-func (c *TestContext) RemoveNode(nodesToKeep []Gravity, remove Gravity) error {
+// joinNode is a helper abstracting logic common to a single node or a multi-node join.
+func (c *TestContext) joinNode(peer, nodeToJoin Gravity, token string, p InstallParam) error {
+
+	cmd := JoinCmd{
+		PeerAddr: peer.Node().PrivateAddr(),
+		Token:    token,
+		Role:     p.Role,
+		StateDir: p.StateDir,
+	}
+
+	c.Logger().WithField("node", nodeToJoin).Info("Join.")
+	ctx, cancel := context.WithTimeout(c.ctx, c.timeouts.Install)
+	defer cancel()
+	err := nodeToJoin.Join(ctx, cmd)
+	return trace.Wrap(err)
+
+}
+
+// Shrink evicts one or more nodes from the cluster
+func (c *TestContext) Shrink(nodesToKeep, nodesToRemove []Gravity) error {
 	if len(nodesToKeep) == 0 {
 		return trace.BadParameter("node list empty")
 	}
+	if len(nodesToRemove) > 1 { // nothing to be removed
+		return nil
+	}
 
 	master := nodesToKeep[0]
+	// Gravity does not support multiple removes in parallel, so loop is
+	// intentionally serialized -- 2020-04 walt
+	// see https://github.com/gravitational/robotest/pull/229#discussion_r428221568
+	for _, node := range nodesToRemove {
+		err := c.RemoveNode(master, node)
+		if err == nil {
+			return trace.Wrap(err)
+		}
+	}
+	return nil
+}
+
+// RemoveNode evicts a singe node from the cluster
+func (c *TestContext) RemoveNode(master, nodeToRemove Gravity) error {
+
+	c.Logger().WithField("node", nodeToRemove).WithField("master", master).Info("Remove.")
 
 	ctx, cancel := context.WithTimeout(c.ctx, c.timeouts.Leave)
 	defer cancel()
 
-	err := master.Remove(ctx, remove.Node().PrivateAddr(), Graceful(!remove.Offline()))
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	err = c.Status(nodesToKeep)
+	err := master.Remove(ctx, nodeToRemove.Node().PrivateAddr(), Graceful(!nodeToRemove.Offline()))
 	return trace.Wrap(err)
 }
