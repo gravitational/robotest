265b8afc08429947efacdc05e25a12a057ba1e25
Walt Della <walt@gravitational.com>
2020-06-10 10:08:46 -0700

    Use the journalctl from $PATH for logging.
    SUSE's journalctl lives at /usr/bin/journalctl whereas Redhat and Debian
    derivatives keep journalctl at /bin/journalctl. Pulling the journalctl
    from root's $PATH seemed like the easiest way to be compatible with
    both.
    
    I recognize this opens up this logging to path injection, but that isn't
    a threat model I'm currently concerned with for robotest.  If an attacker
    can edit root's path the test run is invalid and the nodes need to die.

--- infra/gravity/node_logs.go
+++ infra/gravity/node_logs.go
@@ -9,11 +9,17 @@ import (
 )
 
 func (g *gravity) streamLogs(ctx context.Context) error {
+	// journalctl lives at /usr/bin/journalctl on SUSE and /bin/journalctl on RHEL, Ubuntu, etc.
+	// Thus robotest must either rely on the journalctl from $PATH or plumb through os specific
+	// config. -- walt 2020-06
 	return trace.Wrap(sshutil.Run(ctx, g.Client(), g.Logger().WithField("source", "journalctl"),
-		"sudo /bin/journalctl --follow --output=cat", nil))
+		"sudo journalctl --follow --output=cat", nil))
 }
 
 func (g *gravity) streamStartupLogs(ctx context.Context) error {
+	// journalctl lives at /usr/bin/journalctl on SUSE and /bin/journalctl on RHEL, Ubuntu, etc.
+	// Thus robotest must either rely on the journalctl from $PATH or plumb through os specific
+	// config. -- walt 2020-06
 	return trace.Wrap(sshutil.Run(ctx, g.Client(), g.Logger().WithField("source", "journalctl"),
-		"sudo /bin/journalctl --identifier=startup-script --lines=all --output=cat --follow", nil))
+		"sudo journalctl --identifier=startup-script --lines=all --output=cat --follow", nil))
 }
