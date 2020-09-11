69137f250dd09d3aa8308068ff84c6c7d3a0ed48
Walt Della <walt@gravitational.com>
2020-09-11 02:58:45 +0000

    Split monolithic test into smaller cases.
    This isn't really necessary, but is some cleanup along the way to
    diagnosing the GCP logging issues documented in #245.

--- lib/xlog/all_test.go
+++ lib/xlog/all_test.go
@@ -26,10 +26,9 @@ import (
 )
 
 func TestGcl(t *testing.T) {
-	ctx := context.Background()
-	client, err := NewGCLClient(ctx, "kubeadm-167321")
+	client, err := NewGCLClient(context.Background(), "kubeadm-167321")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("client creation failed: " + err.Error())
 	}
 	defer client.Close()
 
@@ -37,19 +36,33 @@ func TestGcl(t *testing.T) {
 
 	log := NewLogger(client, t, fields)
 
-	_, err = NewGCLClient(ctx, "doesnotexist")
-
 	log.WithFields(logrus.Fields{
 		"wrapped_no_message":   trace.Wrap(err),
 		"wrapped_with_message": trace.Wrap(err, "client error"),
 		"multi_wrap":           trace.Wrap(trace.Wrap(err)),
 		"errorf":               trace.Errorf("there was an error %v", err),
 	}).Error("structured error test")
+}
 
-	short, err := client.Shorten(ctx, `https://console.cloud.google.com/logs/viewer?project=kubeadm-167321&authuser=1&organizationId=419984272859&minLogLevel=0&expandAll=false&resource=global&advancedFilter=resource.type%3D%22global%22%0Alabels.uuid%3D%2231ef6882-0948-4a2c-81df-5bdee81d3c62%22`)
+func TestInvalidProject(t *testing.T) {
+	_, err := NewGCLClient(context.Background(), "doesnotexist")
+	if err == nil {
+		t.Fatal("expected failure when creating a client for a nonexistant project")
+	}
+}
+
+// TestShorten is an integration test for url shortening
+// As of 2020, this is expected to be broken because Google URL shortener has been discontinued.
+// See https://developers.googleblog.com/2018/03/transitioning-google-url-shortener.html for details.
+func TestShorten(t *testing.T) {
+	client, err := NewGCLClient(context.Background(), "kubeadm-167321")
 	if err != nil {
-		log.WithError(err).Error("Failed to shorten URL")
-	} else {
-		log.Info(short)
+		t.Fatal("client creation failed: " + err.Error())
+	}
+	defer client.Close()
+
+	_, err = client.Shorten(context.Background(), `https://console.cloud.google.com/logs/viewer?project=kubeadm-167321&authuser=1&organizationId=419984272859&minLogLevel=0&expandAll=false&resource=global&advancedFilter=resource.type%3D%22global%22%0Alabels.uuid%3D%2231ef6882-0948-4a2c-81df-5bdee81d3c62%22`)
+	if err != nil {
+		t.Fatal("unable to shorten url: " + err.Error())
 	}
 }
